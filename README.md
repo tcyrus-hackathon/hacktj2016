30ad3ceaa68ccfa3e6327b5d055156cf560c6872
TCYRUS <tcyrus@users.noreply.github.com>
2016-02-06 21:51:51 -0500

    Minor Changes

---
+++ README.md
@@ -0,0 +1,3 @@
+# HackTJ 2016 Project - Hangouts + Messenger
+
+Note for Google Hangouts: When setting up the bot, make sure to allow logins for less secure apps at https://www.google.com/settings/security/lesssecureapps
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const login = require("facebook-chat-api");
 const hangoutsBot = require("hangouts-bot");
 const config = require("./config");
 
+var HangoutID;
+
 // Create simple echo bot 
 login(config.messenger, (err, api) => {
     if (err) return console.error(err);
@@ -13,16 +15,17 @@ login(config.messenger, (err, api) => {
 	});
  
 	hangBot.on('message', (from, message) => {
-		console.log(from);
-		HangoutID = from;
-		message = message.split(': ');
-		var id = message[0];
-		message = message.slice(1).join(': ');
-		//id = 100001947199415;
-		api.sendMessage({body: message}, id);
+		if (message == "I AM ROOT") HangoutID = from;
+		else {
+			message = message.split(': ');
+			var id = message[0];
+			message = message.slice(1).join(': ');
+			//id = 100001947199415;
+			api.sendMessage({body: message}, id);
+		}
 	});
 
     api.listen((err, message) => {
-    	hangBot.sendMessage(HangoutID, ': ' + message.body);
+    	hangBot.sendMessage(HangoutID, `${message.senderID}: ${message.body}`);
     });
 });
